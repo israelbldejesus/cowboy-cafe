9fa83205515bbd6f1023826b116522eb994ff54f
IsraelB <israelbldejesus@gmail.com>
2020-02-14 15:17:15 -0600

    Drink2

--- Data/Side.cs
+++ Data/Side.cs
@@ -22,7 +22,7 @@ namespace CowboyCafe.Data
     public abstract class Side
     {
         /// <summary>
-        /// Gets the size of the entree
+        /// Gets the size of the side
         /// </summary>
         public virtual Size Size { get; set; }
 
@@ -32,7 +32,7 @@ namespace CowboyCafe.Data
         public abstract double Price { get; }
 
         /// <summary>
-        /// Gets the calories of the entree
+        /// Gets the calories of the side
         /// </summary>
         public abstract uint Calories { get; }
     }
---
+++ Data/Water.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class which contains the drink Water.
+    /// </summary>
+    public class Water : Drink
+    {
+        /// <summary>
+        /// The calories for the drink Water
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 0;
+                    case Size.Medium:
+                        return 0;
+                    case Size.Large:
+                        return 0 ;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// The price for the drink Water
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 0.12;
+                    case Size.Medium:
+                        return 0.12;
+                    case Size.Large:
+                        return 0.12;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// This will retun/set weather the Drink will have a Lamon.
+        /// </summary>
+        public bool Lemon { get; set; } = true;
+
+        /// <summary>
+        /// This will return the Special Isnnstructions for the Drink.
+        /// </summary>
+        public override List<string> SpecialInstructions
+        {
+            get
+            {
+                List<string> inst = new List<string>();
+
+                if (Lemon) inst.Add("Add Lemon");
+                if (Ice) inst.Add("Hold Ice");
+
+                return inst;
+            }
+        }
+    }
+}
