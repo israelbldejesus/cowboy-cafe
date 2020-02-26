d274ec03920b144dc884789ed97520e11d8a0659
IsraelB <israelbldejesus@gmail.com>
2020-02-26 14:51:33 -0600

    First Test

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// An interface 
+    /// </summary>
+    public interface IOrderItem
+    {
+        /// <summary>
+        /// The price for this order
+        /// </summary>
+        double Price { get; }
+
+        /// <summary>
+        /// The specialninstructions for the order item
+        /// </summary>
+        List<string> SpecialInstructions { get; }
+    }
+}
